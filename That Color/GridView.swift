682fe41183f5da52e56cbbe62d3fc61a89de84c8
polarizz <polarizz39@gmail.com>
2024-05-21 02:18:10 -0700

    Working zoom

--- That Color/GridView.swift
+++ That Color/GridView.swift
@@ -10,145 +10,117 @@ import SwiftUI
 struct GridView: View {
 
     @StateObject private var colorComputation = ColorComputation()
-    @State private var scale: CGFloat = 1.0
+
+    @GestureState var scale: CGFloat = 1.0
+
     @State private var lastScale: CGFloat = 1.0
     @State private var scrollOffset: CGFloat = 0.0
-    @State private var scrollOffsetSmall: CGFloat = 0.0
+    @State private var previousScale: CGFloat = 1.0
+    @State private var velocity: CGFloat = 0.0
+    @State private var fakeScale: CGFloat = 1.0
+    @State private var zoomed: Bool = false
+    @State private var lastTime: TimeInterval = 0.0
 
     let columns = Array(repeating: GridItem(.fixed(UIScreen.main.bounds.width / 10), spacing: 0), count: 10)
-    let prefetchThreshold = 300 // Adjust this to determine how many items before the end to trigger pre-fetching
-
     let columnsSmall = Array(repeating: GridItem(.fixed(UIScreen.main.bounds.width / 6), spacing: 0), count: 6)
-
-    var body: some View {
-        ZStack {
-            VStack {
-                ScrollView {
-                    ZStack {
-                        LazyVGrid(columns: columnsSmall, spacing: 0) {
-                            ForEach(0..<colorComputation.sortedColors.count, id: \.self) { index in
-                                let color = colorComputation.sortedColors[index]
-                                Rectangle()
-                                    .fill(Color(red: color.0, green: color.1, blue: color.2))
-                                    .aspectRatio(1, contentMode: .fit)
-                                    .overlay(
-                                        Text("\(index)")
-                                            .foregroundColor(.white)
-                                            .font(.caption2)
-                                    )
-                                    .onAppear {
-                                        if index >= colorComputation.sortedColors.count - prefetchThreshold {
-                                            colorComputation.computeNextBatch()
-                                        }
-                                    }
-                            }
-                        }
-
-                        GeometryReader { proxy in
-                            let offset = proxy.frame(in: .named("smallScroll")).minY
-                            Color.clear.preference(key: ScrollViewOffsetPreferenceKey.self, value: offset)
-                        }
-                    }
-                }
-                .onAppear {
-                    colorComputation.computeNextBatch()
-                }
-                .scaleEffect(0.61, anchor: .top)
-                .offset(y: scrollOffsetSmall)
-                .coordinateSpace(name: "smallScroll")
-                .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
-                    scrollOffsetSmall = value
-                    print("Small Scroll Offset: \(scrollOffsetSmall)")
-                }
-            }
-
-            VStack {
-                ScrollView {
-                    ZStack {
-                        LazyVGrid(columns: columns, spacing: 0) {
-                            ForEach(0..<colorComputation.sortedColors.count, id: \.self) { index in
-                                let color = colorComputation.sortedColors[index]
-                                Rectangle()
-                                    .fill(Color(red: color.0, green: color.1, blue: color.2))
-                                    .aspectRatio(1, contentMode: .fit)
-                                    .overlay(
-                                        Text("\(index)")
-                                            .foregroundColor(.white)
-                                            .font(.caption2)
-                                    )
-                                    .onAppear {
-                                        if index >= colorComputation.sortedColors.count - prefetchThreshold {
-                                            colorComputation.computeNextBatch()
-                                        }
-                                    }
-                            }
-                        }
-
-                        GeometryReader { proxy in
-                            let offset = proxy.frame(in: .named("scroll")).minY
-                            Color.clear.preference(key: ScrollViewOffsetPreferenceKey.self, value: offset)
-                        }
-                    }
-                }
-                .onAppear {
-                    colorComputation.computeNextBatch()
-                }
-                .opacity(scale < 1.65 ? 0.5 : 0)
-                .animation(.easeInOut(duration: 0.2), value: scale)
-                .coordinateSpace(name: "scroll")
-                .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
-                    scrollOffset = value
-                    print("Scroll Offset: \(scrollOffset)")
-                }
-                .offset(y: scrollOffset)
-            }
-        }
-        .scaleEffect(scale, anchor: .top)
-        .gesture(
-            MagnificationGesture()
-                .onChanged { value in
-                    self.scale = max(self.lastScale * value, 1.0)
-                }
-                .onEnded { _ in
-                    withAnimation {
-                        self.scale = max(self.scale, 1.0)
-                    }
-                    self.lastScale = self.scale
-                }
-        )
-    }
-}
-
-struct GridViewSmall: View {
-    @StateObject private var colorComputation = ColorComputation()
-    let columns = Array(repeating: GridItem(.fixed(UIScreen.main.bounds.width / 6), spacing: 0), count: 6)
     let prefetchThreshold = 300 // Adjust this to determine how many items before the end to trigger pre-fetching
 
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: columns, spacing: 0) {
-                ForEach(0..<colorComputation.sortedColors.count, id: \.self) { index in
-                    let color = colorComputation.sortedColors[index]
-                    Rectangle()
-                        .fill(Color(red: color.0, green: color.1, blue: color.2))
-                        .aspectRatio(1, contentMode: .fit)
-                        .overlay(
-                            Text("\(index)")
-                                .foregroundColor(.white)
-                                .font(.caption2)
-                        )
-                        .onAppear {
-                            if index >= colorComputation.sortedColors.count - prefetchThreshold {
-                                colorComputation.computeNextBatch()
+            ZStack(alignment: .top) {
+                LazyVGrid(columns: columnsSmall, spacing: 0) {
+                    ForEach(0..<colorComputation.sortedColors.count, id: \.self) { index in
+                        let color = colorComputation.sortedColors[index]
+                        Rectangle()
+                            .fill(Color(red: color.0, green: color.1, blue: color.2))
+                            .aspectRatio(1, contentMode: .fit)
+                            .overlay(
+                                Text("\(index)")
+                                    .foregroundColor(.white)
+                                    .font(.caption2)
+                            )
+                            .onAppear {
+                                if index >= colorComputation.sortedColors.count - prefetchThreshold {
+                                    colorComputation.computeNextBatch()
+                                }
                             }
-                        }
+                    }
+                }
+                .scaleEffect(0.61, anchor: .top)
+                .opacity(zoomed ? 1 : 0)
+                .zIndex(1)
+
+                LazyVGrid(columns: columns, spacing: 0) {
+                    ForEach(0..<colorComputation.sortedColors.count, id: \.self) { index in
+                        let color = colorComputation.sortedColors[index]
+                        Rectangle()
+                            .fill(Color(red: color.0, green: color.1, blue: color.2))
+                            .aspectRatio(1, contentMode: .fit)
+                            .overlay(
+                                Text("\(index)")
+                                    .foregroundColor(.white)
+                                    .font(.caption2)
+                            )
+                            .onAppear {
+                                if index >= colorComputation.sortedColors.count - prefetchThreshold {
+                                    colorComputation.computeNextBatch()
+                                }
+                            }
+                    }
+                }
+                .zIndex(0)
+
+                GeometryReader { proxy in
+                    let offset = proxy.frame(in: .named("scroll")).minY
+                    Color.clear.preference(key: ScrollViewOffsetPreferenceKey.self, value: offset)
                 }
             }
         }
         .onAppear {
             colorComputation.computeNextBatch()
         }
-        .scaleEffect(0.61, anchor: .top)
+        .animation(.easeInOut(duration: 0.2), value: scale)
+        .coordinateSpace(name: "scroll")
+        .onPreferenceChange(ScrollViewOffsetPreferenceKey.self) { value in
+            scrollOffset = value
+        }
+        .scaleEffect(scale + (fakeScale - scale), anchor: .top)
+        .gesture(
+            MagnificationGesture()
+                .updating($scale) { value, state, _ in
+                    state = max(lastScale * value, 1.0)
+                }
+                .onChanged { value in
+                    let currentTime = CACurrentMediaTime()
+                    let deltaTime = currentTime - lastTime
+                    let deltaScale = value - previousScale
+
+                    velocity = deltaScale / CGFloat(deltaTime)
+                    lastTime = currentTime
+                    fakeScale = max(lastScale * value, 1.0)
+                    previousScale = value
+
+                    withAnimation(.smooth(duration: 0.3)) {
+                        zoomed = fakeScale >= 1.65
+                    }
+                }
+                .onEnded { _ in
+                    withAnimation(.smooth(duration: 0.39)) {
+                        if velocity > 0 {
+                            zoomed = true
+                            fakeScale = 1.65
+                        } else {
+                            zoomed = false
+                            fakeScale = 1
+                        }
+                    }
+
+                    lastScale = fakeScale
+                    previousScale = 1.0
+                    velocity = 0
+                }
+        )
+        .animation(.smooth(duration: 0.39), value: scale)
     }
 }
 
